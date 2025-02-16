045c52a0869561549589aa481f6f5e78e54e583a
xiyuxianqd <118612253+xiyuxianqd@users.noreply.github.com>
2025-02-16 19:18:34 +0800

    github_pull_test

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,5 +3,6 @@ public class hello {
         System.out.println("master");
         System.out.println("masyer2");
         System.out.println("HOTFIX");
+           System.out.println("这是github改的"); 
     }
 }
